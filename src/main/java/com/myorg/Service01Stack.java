9623a7bf1dab9b05e46beb60e8b72e7dd8339860
Vitória <vitoriagcf@hotmail.com>
2022-01-25 00:11:14 -0300

    Building Java Microservices with AWS ECS and Fargate

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -36,7 +36,7 @@ public class Service01Stack extends Stack {
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
                                 .containerName("aws-project01")
-                                .image(ContainerImage.fromRegistry("vigcfdev/aws_project01:1.2.0"))
+                                .image(ContainerImage.fromRegistry("vigcfdev/aws_project01:1.3.0"))
                                 .containerPort(8080)
                                 .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                         .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
